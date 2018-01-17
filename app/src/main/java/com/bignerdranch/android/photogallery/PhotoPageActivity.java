414c4c917ddd86b0919812e24f968b6a29c401fd
UBanit <apollovin777@gmail.com>
2018-01-17 15:50:27 +0200

    30.1

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageActivity.java
@@ -4,12 +4,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 /**
  * Created by UBanit on 1/17/2018.
  */
 
 public class PhotoPageActivity extends SingleFragmentActivity {
+
+    public interface OnBackPressedListener {
+        public boolean onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fm = getSupportFragmentManager();
+        OnBackPressedListener backPressedListener = null;
+        boolean handled = false;
+        for (Fragment fragment: fm.getFragments()) {
+            if (fragment instanceof  OnBackPressedListener) {
+                backPressedListener = (OnBackPressedListener) fragment;
+                break;
+            }
+        }
+
+        if (backPressedListener != null) {
+            handled = backPressedListener.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+
+        if (handled){
+            super.onBackPressed();
+        }
+    }
+
     public static Intent newIntent(Context context, Uri photoPageUri) {
         Intent i = new Intent(context,PhotoPageActivity.class);
         i.setData(photoPageUri);
@@ -20,4 +49,6 @@ public class PhotoPageActivity extends SingleFragmentActivity {
     protected Fragment createFragment() {
         return PhotoPageFragment.newInstance(getIntent().getData());
     }
+
+
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -1,24 +1,32 @@
 package com.bignerdranch.android.photogallery;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
-public class PhotoPageFragment extends VisibleFragment {
+import com.bignerdranch.android.photogallery.PhotoPageActivity.OnBackPressedListener;
+
+public class PhotoPageFragment extends VisibleFragment implements OnBackPressedListener {
+    private static final String TAG = "PhotoPageFragment";
     private static final String ARG_URI = "photo_page_url";
 
     private Uri mUri;
     private WebView mWebView;
     private ProgressBar mProgressBar;
+
+
 
     public static PhotoPageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -63,8 +71,20 @@ public class PhotoPageFragment extends VisibleFragment {
         });
 
         mWebView.setWebViewClient(new WebViewClient());
+
         mWebView.loadUrl(mUri.toString());
         return v;
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+            return false;
+        } else {
+            return true;
+        }
+
     }
 }
 
